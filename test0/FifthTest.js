1c3af4445761ba2dbd8d4861b20d53e9dd31b525
Gonzo <renzomoripanesi@gmail.com>
2021-12-07 18:07:33 -0300

    test

---
+++ test0/FifthTest.js
@@ -0,0 +1,32 @@
+import {Selector} from 'testcafe';
+
+const triedTCCheck = Selector("input#tried-test-cafe")
+const slider = Selector("div#slider")
+const populateButton = Selector("input#populate")
+const developername = Selector ("#developer-name")
+const submitButton = Selector("#submit-button")
+const labelMessage = Selector("#article-header")
+let nombre="Renzo Mori"
+
+fixture("Assertions")
+.page("http://devexpress.github.io/testcafe/example/")
+
+    .beforeEach(async t => {
+        await t 
+        .setTestSpeed(0.25)
+        .maximizeWindow();
+
+    })
+
+    test("TC01 - Expectation", async t => {
+        await t
+        .hover(populateButton)
+        .expect(developername.value).eql('','input is empty')
+        .typeText(developername,nombre)
+        .expect(developername.value).eql(nombre,'input is not empty')
+        console.log(developername.value);
+        await t
+        .click(submitButton)
+        .expect(labelMessage.textContent).contains('Thank you')
+    })
+    
--- test0/FourthTest.js
+++ test0/FourthTest.js
@@ -2,14 +2,26 @@ import {Selector} from 'testcafe';
 
 const triedTCCheck = Selector("input#tried-test-cafe")
 const slider = Selector("div#slider")
+const populateButton = Selector("input#populate")
 
 fixture("Arrastrar elementos")
 .page("http://devexpress.github.io/testcafe/example/")
 
-    test("TC01 - Primera Prueba", async t => {
-        await t
+    .beforeEach(async t => {
+        await t 
         .setTestSpeed(0.25)
+        .maximizeWindow();
+
+    })
+
+    test("TC01 - Primera Prueba", async t => {
+        await t 
         .click(triedTCCheck)
         .drag(slider,360,0,{offsetX:10,offsetY:0})
         .wait(20000)
-    }).only
+    })
+
+    test("TC02 - Hover", async t => {
+        await t 
+        .hover(populateButton)
+    })
